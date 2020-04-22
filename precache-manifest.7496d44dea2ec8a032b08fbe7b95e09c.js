4e6c836ae7fb54fdfb965c4dcb12bd91ac05c1bc
skymoon103 <shrestha.anish@outlook.com>
2020-04-22 00:00:37 -0500

    Updates

---
+++ precache-manifest.7496d44dea2ec8a032b08fbe7b95e09c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea2680874c4b25664e410046ae85b93",
+    "url": "/react-FaceRecognition/index.html"
+  },
+  {
+    "revision": "fd10ca9fc152e1ec7efa",
+    "url": "/react-FaceRecognition/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "16f5306f9b0ad24af5e6",
+    "url": "/react-FaceRecognition/static/css/main.8750d9c9.chunk.css"
+  },
+  {
+    "revision": "fd10ca9fc152e1ec7efa",
+    "url": "/react-FaceRecognition/static/js/2.ef7e4cec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-FaceRecognition/static/js/2.ef7e4cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f5306f9b0ad24af5e6",
+    "url": "/react-FaceRecognition/static/js/main.7ed86a5d.chunk.js"
+  },
+  {
+    "revision": "9bb7679ad5b80d82ba5b",
+    "url": "/react-FaceRecognition/static/js/runtime-main.66059fbc.js"
+  }
+]);
